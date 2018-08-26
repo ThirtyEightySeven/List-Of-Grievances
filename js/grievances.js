22a9b73fb9a7721f48f79c1ea3b708a0451cf4fe
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-08-26 15:43:14 -0400

    Add shuffle function

--- js/grievances.js
+++ js/grievances.js
@@ -1,9 +1,18 @@
 const grievances = [
-
+  
 ];
 
-function displayGrievances() {
-    grievances.forEach(function (grievance) {
+// Shuffle using Fisher-Yates algorithm
+function shuffleGrievances() {
+    for (let i = grievances.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [grievances[i], grievances[j]] = [grievances[j], grievances[i]];
+    }
+}
 
+function displayGrievances() {
+    shuffleGrievances();
+    grievances.forEach(function (grievance) {
+        console.log(grievance);
     });
 }
