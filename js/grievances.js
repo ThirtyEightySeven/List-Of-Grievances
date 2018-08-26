89e2c6e190b4d99264760a7920856b30ef5fd732
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-08-26 16:08:19 -0400

    Add image stubs

--- js/grievances.js
+++ js/grievances.js
@@ -4,11 +4,18 @@ const grievances = [
     'test 3',
 ];
 
+const grievance_images = [
+    'url 1',
+    'url 2',
+    'url 3',
+]
+
 // Shuffle using Fisher-Yates algorithm
 function shuffleGrievances() {
     for (let i = grievances.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [grievances[i], grievances[j]] = [grievances[j], grievances[i]];
+        [grievance_images[i], grievance_images[j]] = [grievance_images[j], grievance_images[i]];
     }
 }
 
@@ -18,3 +25,4 @@ function displayGrievances() {
         document.write('<div class="container-fluid content-pane">' + grievance + '</div>')
     });
 }
+
