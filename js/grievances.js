48c99be92b0ec980ec6381771af69c559f8f3f8d
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-08-27 09:07:01 -0400

    Convert parallel arrays into objects

--- js/grievances.js
+++ js/grievances.js
@@ -1,40 +1,51 @@
-const grievances = [
-    'Midnight Oil closes at 5pm on weekdays.',
-    'Orientation lines start an hour before event starts.',
-    'The Quarter Mile isn\'t well lit at night.',
-    'Tech Crew blasts music at night around dorms.',
-    'Half of Gracies is always blocked off due to spills.',
-    'Gracies lacks silverware and cups during rushes.',
+const grievances = [{
+        description: 'Midnight Oil closes at 5pm on weekdays.',
+        image: 'https://www.rit.edu/fa/diningservices/sites/rit.edu.fa.diningservices/files/styles/juicebox_large/public/Midnight_0620.jpg?itok=rmsF2S4p'
+    },
+    {
+        description: 'Orientation lines start an hour before event starts.',
+        image: 'http://www.rit.edu/news/lib/filelib/200708/new_students.jpg'
+    },
+    {
+        description: 'The Quarter Mile isn\'t well lit at night.',
+        image: 'http://www.ntid.rit.edu/cmmsdb/sites/www.ntid.rit.edu.cmmsdb/files/photos/big-shot_rit0024_900.jpg'
+    },
+    {
+        description: 'Tech Crew blasts music at night around dorms.',
+        image: 'http://www.b-metro.co.zw/wp-content/uploads/2018/08/loud-music.jpg'
+    },
+    {
+        description: 'Half of Gracies is always blocked off due to spills.',
+        image: 'https://igx.4sqi.net/img/general/699x268/11977479_bNWnzBZppBoEj_bz0sbVQYE1_QJgmCvFY8ISKGBqmlQ.jpg'
+    },
+    {
+        description: 'Gracies lacks silverware and cups during rushes.',
+        image: 'https://www.rit.edu/fa/diningservices/sites/rit.edu.fa.diningservices/files/styles/juicebox_large/public/Gracies_7965.jpg?itok=CntFkdxe'
+    },
 ];
 
-const grievance_images = [
-    'https://www.rit.edu/fa/diningservices/sites/rit.edu.fa.diningservices/files/styles/juicebox_large/public/Midnight_0620.jpg?itok=rmsF2S4p',
-    'http://www.rit.edu/news/lib/filelib/200708/new_students.jpg',
-    'http://www.ntid.rit.edu/cmmsdb/sites/www.ntid.rit.edu.cmmsdb/files/photos/big-shot_rit0024_900.jpg',
-    'http://www.b-metro.co.zw/wp-content/uploads/2018/08/loud-music.jpg',
-    'https://igx.4sqi.net/img/general/699x268/11977479_bNWnzBZppBoEj_bz0sbVQYE1_QJgmCvFY8ISKGBqmlQ.jpg',
-    'https://www.rit.edu/fa/diningservices/sites/rit.edu.fa.diningservices/files/styles/juicebox_large/public/Gracies_7965.jpg?itok=CntFkdxe',
-];
-
+console.log(grievances[0]);
 
 // Shuffle using Fisher-Yates algorithm
 function shuffleGrievances() {
     for (let i = grievances.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [grievances[i], grievances[j]] = [grievances[j], grievances[i]];
-        [grievance_images[i], grievance_images[j]] = [grievance_images[j], grievance_images[i]];
     }
 }
 
 function displayGrievances() {
     shuffleGrievances();
     for (let i = 0; i < grievances.length; i++) {
-        $('#containers').prepend('<div class="container-fluid content-pane grievance-container" style="' + 'background-image: url(' + grievance_images[i] + ');"> <p>' + grievances[i] + '</p></div>')
+        $('#containers').prepend('<div class="container-fluid content-pane grievance-container" style="' + 'background-image: url(' + grievances[i].image + ');"> <p>' + grievances[i].description + '</p></div>')
     }
 }
 
 function addGrievance(title, grievance, img, isNew) {
-    grievances.unshift(grievance);
+    grievances.unshift({
+        description: grievance,
+        image: null
+    });
     if (!img) findImage(title, isNew);
 }
 
@@ -51,7 +62,7 @@ function findImage(title, isNew) {
 }
 
 function addImage(img) {
-    grievance_images.unshift(img);
+    grievances[0].image = img;
 }
 
 $(document).ready(function () {
@@ -64,5 +75,5 @@ $(document).ready(function () {
 });
 
 function createContainer() {
-   $('#containers').prepend('<div class="container-fluid content-pane grievance-container title-text" style="' + 'background-image: url(' + grievance_images[0] + ');"><p>' + grievances[i] + '</p></div>')
+    $('#containers').prepend('<div class="container-fluid content-pane grievance-container" style="' + 'background-image: url(' + grievances[0].image + ');"><p>' + grievances[0].description + '</p></div>')
 }
