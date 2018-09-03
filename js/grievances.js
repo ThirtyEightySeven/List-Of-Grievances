135129fdaba073594210d60025a01155a21c81d8
Michael Van Leeuwen <michaeljvanleeuwen@gmail.com>
2018-09-03 15:38:24 -0400

    Add new style sets

--- js/grievances.js
+++ js/grievances.js
@@ -11,7 +11,7 @@ function shuffleGrievances() {
 function displayGrievances() {
     shuffleGrievances();
     for (let i = 0; i < grievances.length; i++) {
-        $('#entry').prepend('<li class="cards__item"><div class="card"><img class="card__image" src="' + grievances[i].image + '"></img><div class="card__content"><div class="card__title">' + grievances[i].title + '</div><p class="card__text">' + grievances[i].description + '</p><button class="btn btn--block">Button</button></div></div></li>');
+        $('#entry').prepend('<li class="cards__item"><div class="card"><img class="card__image" src="' + grievances[i].image + '"></img><div class="card__content"><div class="card__title hvr-skew-forward">' + grievances[i].title + '</div><p class="card__text">' + grievances[i].description + '</p><button class="btn btn--block">Button</button></div></div></li>');
     }
 }
 
